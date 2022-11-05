6507f5089552e2fc6662688f1e690f799cab5241
10high <99827666+10high@users.noreply.github.com>
2022-11-05 17:18:34 +0100

    enlarges website/git popup links

--- modules/cardBuilder.js
+++ modules/cardBuilder.js
@@ -23,8 +23,8 @@ const buildCardElements = portfolioItem => {
   const card__title = document.createElement("h3");
 
   //builds website SVG icon
-  card__popupWebsiteIconSVG.setAttribute("width", "16");
-  card__popupWebsiteIconSVG.setAttribute("height", "16");
+  card__popupWebsiteIconSVG.setAttribute("width", "24");
+  card__popupWebsiteIconSVG.setAttribute("height", "24");
   card__popupWebsiteIconSVG.setAttribute("fill", "currentColor");
   card__popupWebsiteIconSVG.setAttribute("viewBox", "0 0 16 16");
   card__popupWebsiteIconPath1.setAttribute("fill-rule", "evenodd");
@@ -34,8 +34,8 @@ const buildCardElements = portfolioItem => {
   card__popupWebsiteIconSVG.append(card__popupWebsiteIconPath1, card__popupWebsiteIconPath2);
   card__popupWebsiteLink.append(card__popupWebsiteIconSVG);
   //builds Git SVG icon
-  card__popupGitIconSVG.setAttribute("width", "16");
-  card__popupGitIconSVG.setAttribute("height", "16");
+  card__popupGitIconSVG.setAttribute("width", "24");
+  card__popupGitIconSVG.setAttribute("height", "24");
   card__popupGitIconSVG.setAttribute("fill", "currentColor");
   card__popupGitIconSVG.setAttribute("viewBox", "0 0 16 16");
   card__popupGitIconPath.setAttribute("d", "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z");
