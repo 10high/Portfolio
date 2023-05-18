1be8e565f3fec3eb33630e65b9de316ae926d022
Michael Waaler <high10s@michaellaptop.fritz.box>
2023-05-18 19:30:21 +0200

    updates CV href

--- modules/bioCopy.js
+++ modules/bioCopy.js
@@ -9,7 +9,7 @@ export const buildBioCopy = {
     I currently like working with *React, PostCSS,* and *Vite,* but I'm also familiar with *Parcel* and *BEM notation.*  
     I'm also fluent in *English* and *German.*
     Check out my <link=https://www.linkedin.com/in/michael-waaler-55854717/urlend>*LinkedIn*</link> profile for more info about me.
-    You can also *download* my CV <link=../Portfolio/documents/MichaelWaaler_CV_resume.pdfurlend>*here.*</link>`,
+    You can also *download* my CV <link=../Portfolio/documents/01_Michael_Waaler_Junior_Frontend_Developer.pdfurlend>*here.*</link>`,
     formatCopy() {
         this.copy = this.copy.replace(/(\*.*?\*)/gm, `<span class="boldText">$1</span>`);
         this.copy = this.copy.replace(/\*/gm, "");
