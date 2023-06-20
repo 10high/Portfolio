6b6408113407d97b8cda945f2820f8e5089f42e8
Michael Waaler <michael@flyingtens.com>
2023-06-20 10:33:50 +0200

    adds Simple Gallery

--- modules/portfolioItems.js
+++ modules/portfolioItems.js
@@ -1,83 +1,137 @@
-
 export const portfolioItems = [
-    {
-        "heading": "Fluid Typography Calculator",
-        "category": "Web App",
-        "date": 220900,
-        "imageSrc": "url(./images/portfolioThumbnails/FluidTypographyCalculator_548x480px.webp)",
-        "websiteLink": "https://10high.github.io/Fluid-Typography-Calculator/",
-        "githubLink": "https://github.com/10high/Fluid-Typography-Calculator",
-        "description": "A personal project I use for building responsive pages with clamp()",
-        "descriptionList": ["Strict form input handling", "BEM"]
-    },
-    {
-        "heading": "Dr. Bergerhof",
-        "category": "Website",
-        "date": 220500,
-        "imageSrc": "url(./images/portfolioThumbnails/BergerhoffArtzPraxis_671x480px.webp)",
-        "websiteLink": "https://www.praxisbergerhoff.de/",
-        "githubLink": "https://github.com/10high/BergerhoffWebsite",
-        "description": "The website for a local doctor's practice.",
-        "descriptionList": ["Basic CSS animations", "Distinct mobile/desktop implementations with grid & flexbox"]
-    },
-    {
-        "heading": "Cult Manager Tycoon",
-        "category": "Mobile Game",
-        "date": 200700,
-        "imageSrc": "url(./images/portfolioThumbnails/CultManageTycoon_854x417px.webp)",
-        "websiteLink": "https://cheapshotgames.itch.io/cult-manager-tycoon",
-        "githubLink": "",
-        "description": "A mobile game for Android. Originally published on Play Store, now archived on itch.io.",
-        "descriptionList": ["Made with Unity & Playmaker"]
-    },
-    {
-        "heading": "Pod Request Landing Page",
-        "category": "Challenge",
-        "date": 220800,
-        "imageSrc": "url(./images/portfolioThumbnails/PodRequestLandingPage.webp)",
-        "websiteLink": "https://10high.github.io/FrontendMentorPodRequest/",
-        "githubLink": "https://github.com/10high/FrontendMentorPodRequest",
-        "description": "A Frontend Mentor challenge.",
-        "descriptionList": ["Basic JS form error handling", "Pixel perfect", "BEM CSS"]
-    },
-    {
-        "heading": "Portfolio Page",
-        "category": "Website",
-        "date": 221000,
-        "imageSrc": "url(./images/portfolioThumbnails/PortfolioPage.webp)",
-        "websiteLink": "#",
-        "githubLink": "https://github.com/10high/Portfolio",
-        "description": "This portfolio page!",
-        "descriptionList": ["Factory functions", "Dynamic JavaScript", "RegEx content formatting", "Modular structure", "PostCSS / BEM"]
-    },
-    {
-        "heading": "Clock App",
-        "category": "Challenge",
-        "date": 230207,
-        "imageSrc": "url(./images/portfolioThumbnails/clockapp_854x474px.webp)",
-        "websiteLink": "https://clinquant-cajeta-675914.netlify.app/",
-        "githubLink": "https://github.com/10high/ClockApp",
-        "description": "Frontend Mentor Challenge",
-        "descriptionList": ["API, async/await", "Netlify serverless function (keeps API key secret)", "Vanilla JavaScript", "BEM CSS", "Parcel bundler"]
-    },
-    {
-        "heading": "Github Devfinder App",
-        "category": "Challenge",
-        "date": 221200,
-        "imageSrc": "url(./images/portfolioThumbnails/githubdevfinderapp_854x478px.webp)",
-        "websiteLink": "https://10high.github.io/GithubSearchApp/",
-        "githubLink": "https://github.com/10high/GithubSearchApp",
-        "description": "Frontend Mentor Challenge",
-        "descriptionList": ["API, async/await", "Light/dark mode w/ prefers-color-scheme", "Vanilla JavaScript", "BEM CSS", "Custom properties", "Parcel bundler"]
-    },
-    {
-        "heading": "Placeholder Search App",
-        "category": "Web App",
-        "date": 230401,
-        "imageSrc": "url(./images/portfolioThumbnails/FilemakerPlaceholderSearchApp.png)",
-        "websiteLink": "",
-        "githubLink": "https://github.com/10high/FilemakerPlaceholderSearch",
-        "description": "A Filemaker app recreated as Web app for querying placeholders in the Filemaker database.",
-        "descriptionList": ["Filemaker REST API, async/await", "Create React App with class components", "Requests over PHP proxy", "PHP fmRESTor library", "CSS modules"]
-    }
-]
+  {
+    heading: "Fluid Typography Calculator",
+    category: "Web App",
+    date: 220900,
+    imageSrc:
+      "url(./images/portfolioThumbnails/FluidTypographyCalculator_548x480px.webp)",
+    websiteLink: "https://10high.github.io/Fluid-Typography-Calculator/",
+    githubLink: "https://github.com/10high/Fluid-Typography-Calculator",
+    description:
+      "A personal project I use for building responsive pages with clamp()",
+    descriptionList: ["Strict form input handling", "BEM"],
+  },
+  {
+    heading: "Dr. Bergerhof",
+    category: "Website",
+    date: 220500,
+    imageSrc:
+      "url(./images/portfolioThumbnails/BergerhoffArtzPraxis_671x480px.webp)",
+    websiteLink: "https://www.praxisbergerhoff.de/",
+    githubLink: "https://github.com/10high/BergerhoffWebsite",
+    description: "The website for a local doctor's practice.",
+    descriptionList: [
+      "Basic CSS animations",
+      "Distinct mobile/desktop implementations with grid & flexbox",
+    ],
+  },
+  {
+    heading: "Cult Manager Tycoon",
+    category: "Mobile Game",
+    date: 200700,
+    imageSrc:
+      "url(./images/portfolioThumbnails/CultManageTycoon_854x417px.webp)",
+    websiteLink: "https://cheapshotgames.itch.io/cult-manager-tycoon",
+    githubLink: "",
+    description:
+      "A mobile game for Android. Originally published on Play Store, now archived on itch.io.",
+    descriptionList: ["Made with Unity & Playmaker"],
+  },
+  {
+    heading: "Pod Request Landing Page",
+    category: "Challenge",
+    date: 220800,
+    imageSrc: "url(./images/portfolioThumbnails/PodRequestLandingPage.webp)",
+    websiteLink: "https://10high.github.io/FrontendMentorPodRequest/",
+    githubLink: "https://github.com/10high/FrontendMentorPodRequest",
+    description: "A Frontend Mentor challenge.",
+    descriptionList: [
+      "Basic JS form error handling",
+      "Pixel perfect",
+      "BEM CSS",
+    ],
+  },
+  {
+    heading: "Portfolio Page",
+    category: "Website",
+    date: 221000,
+    imageSrc: "url(./images/portfolioThumbnails/PortfolioPage.webp)",
+    websiteLink: "#",
+    githubLink: "https://github.com/10high/Portfolio",
+    description: "This portfolio page!",
+    descriptionList: [
+      "Factory functions",
+      "Dynamic JavaScript",
+      "RegEx content formatting",
+      "Modular structure",
+      "PostCSS / BEM",
+    ],
+  },
+  {
+    heading: "Clock App",
+    category: "Challenge",
+    date: 230207,
+    imageSrc: "url(./images/portfolioThumbnails/clockapp_854x474px.webp)",
+    websiteLink: "https://clinquant-cajeta-675914.netlify.app/",
+    githubLink: "https://github.com/10high/ClockApp",
+    description: "Frontend Mentor Challenge",
+    descriptionList: [
+      "API, async/await",
+      "Netlify serverless function (keeps API key secret)",
+      "Vanilla JavaScript",
+      "BEM CSS",
+      "Parcel bundler",
+    ],
+  },
+  {
+    heading: "Github Devfinder App",
+    category: "Challenge",
+    date: 221200,
+    imageSrc:
+      "url(./images/portfolioThumbnails/githubdevfinderapp_854x478px.webp)",
+    websiteLink: "https://10high.github.io/GithubSearchApp/",
+    githubLink: "https://github.com/10high/GithubSearchApp",
+    description: "Frontend Mentor Challenge",
+    descriptionList: [
+      "API, async/await",
+      "Light/dark mode w/ prefers-color-scheme",
+      "Vanilla JavaScript",
+      "BEM CSS",
+      "Custom properties",
+      "Parcel bundler",
+    ],
+  },
+  {
+    heading: "Placeholder Search App",
+    category: "Web App",
+    date: 230401,
+    imageSrc:
+      "url(./images/portfolioThumbnails/FilemakerPlaceholderSearchApp.png)",
+    websiteLink: "",
+    githubLink: "https://github.com/10high/FilemakerPlaceholderSearch",
+    description:
+      "A Filemaker app recreated as Web app for querying placeholders in the Filemaker database.",
+    descriptionList: [
+      "Filemaker REST API, async/await",
+      "Create React App with class components",
+      "Requests over PHP proxy",
+      "PHP fmRESTor library",
+      "CSS modules",
+    ],
+  },
+  {
+    heading: "Simple Gallery",
+    category: "Practice",
+    date: 230616,
+    imageSrc: "url(./images/portfolioThumbnails/simplegallery_854x825px.webp)",
+    websiteLink: "https://flyingtens.com/SimpleGallery/",
+    githubLink: "https://github.com/10high/SimpleGallery",
+    description: "A React and React Router practice project",
+    descriptionList: [
+      "React / React Router",
+      "Flickr API / PHP fetch proxy",
+      "Built in Vite",
+      "PostCSS / CSS modules",
+    ],
+  },
+];
